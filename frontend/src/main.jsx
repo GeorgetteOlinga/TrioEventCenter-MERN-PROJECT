af565ab42a4d81d6b83b76a50db51d0b3f58b93b
git <olingageorgette1@gmail.com>
2024-01-17 23:48:16 -0500

    16

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,13 @@
-import ReactDOM from "react-dom/client";
-import App from "./pages/App.jsx";
-import "./styles/index.css";
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './pages/App/App.jsx'
+import './index.css'
+import { BrowserRouter as Router } from 'react-router-dom'
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Router >
+      <App />
+    </Router>
+  </React.StrictMode>,
+)
